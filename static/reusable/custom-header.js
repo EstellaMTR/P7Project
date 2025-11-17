d66de332728b7c82c6f20eb9770893e6edc1b1d7
Estella <estellatr98@gmail.com>
2025-11-17 12:07:28 +0100

    edited button paths

--- static/reusable/custom-header.js
+++ static/reusable/custom-header.js
@@ -118,23 +118,13 @@ template.innerHTML = `
     <header>
         <nav>
             <ul class="nav-bar">
-<<<<<<< HEAD:public/reusable/custom-header.js
                 <button class="fast-exit" id="exit-button" aria-label="Fast Exit"><i class="fa-solid fa-xmark"></i></button>
-                <li class="nav-button"><a href='/public/index.html'><i class="fa-solid fa-house"></i><br>Home</a></li>
-                <li class="nav-button"><a href='/public/users/chat/chat.html'><i class="fa-solid fa-comments"></i><br>Chat</a></li>
-                <li class="nav-button"><a href="/public/resources/resources.html"><i class="fa-solid fa-sitemap"></i><br>Resources</a></li>
-                <li class="nav-button"><a href="/public/articles/articles.html"><i class="fa-solid fa-lightbulb"></i><br>Articles</a></li>
-                <li class="nav-button"><a href="/public/stories/stories.html"><i class="fa-solid fa-heart"></i><br>Stories</a></li>
-                <li id="login-button" class="pro-login"><a href="/public/volunteers/login/login.html"><i class="fa-solid fa-user"></i><br><p>Professional<br>Login</p></a></li>
-=======
-                <button class="fast-exit" id="exit-button" aria-label="Fast Exit">x</button>
-                <li><a href='/'><i class="fa-solid fa-house"></i><br>Home</a></li>
-                <li><a href='/chat'><i class="fa-solid fa-comments"></i><br>Chat</a></li>
-                <li><a href='/resources'><i class="fa-solid fa-sitemap"></i><br>Resources</a></li>
-                <li><a href='/articles'><i class="fa-solid fa-lightbulb"></i><br>Articles</a></li>
-                <li><a href='/stories'><i class="fa-solid fa-heart"></i><br>Stories</a></li>
-                <li id="login-button" class="pro-login"><a href="/login"><i class="fa-solid fa-user"></i><br><p>Here as a professional?<br>Login to chat</p></a></li>
->>>>>>> main:static/reusable/custom-header.js
+                <li class="nav-button"><a href='{{ url_for('static', filename='/public/index.html'><i class="fa-solid fa-house"></i><br>Home</a></li>
+                <li class="nav-button"><a href="{{ url_for('static', filename='/public/users/chat/chat.html') }}"><i class="fa-solid fa-comments"></i><br>Chat</a></li>
+                <li class="nav-button"><a href="{{ url_for('static', filename='/public/resources/resources.html') }}"><i class="fa-solid fa-sitemap"></i><br>Resources</a></li>
+                <li class="nav-button"><a href="{{ url_for('static', filename='/public/articles/articles.html') }}"><i class="fa-solid fa-lightbulb"></i><br>Articles</a></li>
+                <li class="nav-button"><a href="{{ url_for('static', filename='/public/stories/stories.html') }}"><i class="fa-solid fa-heart"></i><br>Stories</a></li>
+                <li id="login-button" class="pro-login"><a href="{{ url_for('static', filename='/public/volunteers/login/login.html') }}"><i class="fa-solid fa-user"></i><br><p>Professional<br>Login</p></a></li>
             </ul>
         </nav> 
     </header>
